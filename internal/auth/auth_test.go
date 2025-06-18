2c4506141a79a94d96993cd9819a080968784ae7
Noah <volpy2000@gmail.com>
2025-06-18 11:36:34 +0200

    teset will fail

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+	"fmt"
+)
+
+func TestGetBearerToken(t *testing.T) {
+	tests := []struct {
+		name      string
+		headers   http.Header
+		wantToken string
+		wantErr   bool
+	}{
+		{
+			name: "Valid Bearer token",
+			headers: http.Header{
+				"Authorization": []string{"Api valid_token"},
+			},
+			wantToken: "valid_token",
+			wantErr:   false,
+		},
+		{
+			name:      "Missing Authorization header",
+			headers:   http.Header{},
+			wantToken: "",
+			wantErr:   true,
+		},
+		{
+			name: "Malformed Authorization header",
+			headers: http.Header{
+				"Authorization": []string{"InvalidApiKey token"},
+			},
+			wantToken: "",
+			wantErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotToken, err := GetAPIKey(tt.headers)
+			fmt.Println(gotToken)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetBearerToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotToken != tt.wantToken {
+				t.Errorf("GetBearerToken() gotToken = %v, want %v", gotToken, tt.wantToken)
+			}
+		})
+	}
+}
+
+// GetAPIKey -
+// func GetAPIKey(headers http.Header) (string, error) {
+// 	authHeader := headers.Get("Authorization")
+// 	if authHeader == "" {
+// 		return "", ErrNoAuthHeaderIncluded
+// 	}
+// 	splitAuth := strings.Split(authHeader, " ")
+// 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+// 		return "", errors.New("malformed authorization header")
+// 	}
+//
+// 	return splitAuth[1], nil
+// }
+
